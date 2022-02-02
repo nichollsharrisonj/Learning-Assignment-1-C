83c363daacf022ac47708a4395942880b6a29cd1
Harrison Nicholls <66330102+nichollsharrisonj@users.noreply.github.com>
2022-02-02 15:44:53 -0800

    Update list.c

--- list.c
+++ list.c
@@ -24,16 +24,17 @@ int main (){
    float sum = 0;
 
    for(int  i = 0 ; i < list_length ; i++ ) {
+
       vals[i] = rand() % max;
       sum += vals[i];
    }
 
    for(int  i = 0 ; i < list_length ; i++ ) {
-      printf("%f ",vals[i]);
+      printf("%.0f ",vals[i]);
    }
 
    float mean = sum / list_length;
-   printf("\nThe mean is: \n %f", mean);
+   printf("\nThe mean is: \n %.2f", mean);
 
    return 0;
 }
